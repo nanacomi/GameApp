29b863cb8bfdd829501974c23f4d9b0db09000ea
Masato Ebisawa <j0td5ta0d1mt@icloud.com>
2019-07-07 18:58:30 +0900

    refactor keyboard inputs

--- client/src/main.js
+++ client/src/main.js
@@ -1,52 +1,11 @@
-const keyArray            = new Array(10);
-const keyArrayUp          = new Array(10);
-
-let key_repeat_x_plus   = false;
-let key_repeat_x_minus  = false;
-let key_repeat_z_plus   = false;
-let key_repeat_z_minus  = false;
-
-const KEY_LEFT  = 0;
-const KEY_UP    = 1;
-const KEY_RIGHT = 2;
-const KEY_DOWN  = 3;
-
-document.addEventListener("keydown", function(e) {
-    switch(e.keyCode) {
-    case 37: keyArray[0] = true; break;//left
-    case 38: keyArray[1] = true; break;//up
-    case 39: keyArray[2] = true; break;//right
-    case 40: keyArray[3] = true; break;//down
-    case 88: keyArray[4] = true; break;//x
-    case 90: keyArray[5] = true; break;//z
-    case 83: keyArray[6] = true; break;//s
-    case 65: keyArray[7] = true; break;//a
-    case 81: keyArray[8] = true; break;
-    case 87: keyArray[9] = true; break;
-    }
-}, false);
-
-document.addEventListener("keyup", function(e) {
-    switch(e.keyCode) {
-    case 37: keyArrayUp[0] = true; break;
-    case 38: keyArrayUp[1] = true; break;   
-    case 39: keyArrayUp[2] = true; break;
-    case 40: keyArrayUp[3] = true; break;
-    case 88: keyArrayUp[4] = true; break;
-    case 90: keyArrayUp[5] = true; break;
-    case 83: keyArrayUp[6] = true; break;
-    case 65: keyArrayUp[7] = true; break;
-    case 81: keyArrayUp[8] = true; break;
-    case 67: keyArrayUp[9] = true; break;
-    }
-}, false);
-
 const MAX = 50;
 class GameApp {
-    constructor() {
+    constructor(controller) {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        this.controller = controller;
         
         this.cubes = [];
     }
@@ -84,6 +43,27 @@ class GameApp {
     }
 }
 
+class Controller {
+    constructor() {
+        this.moveKeys = new Map([[37, false], [38, false], [39, false], [40, false]]);
+    }
+
+    handleKeyInput() {
+        document.addEventListener('keydown', e => {
+            e.preventDefault();
+            if (this.moveKeys.has(e.keyCode) && !this.moveKeys.get(e.keyCode)) {
+                this.moveKeys.set(e.keyCode, true);
+            }
+        });
+        document.addEventListener('keyup', e => {
+            e.preventDefault();
+            if (this.moveKeys.has(e.keyCode) && this.moveKeys.get(e.keyCode)) {
+                this.moveKeys.set(e.keyCode, false);
+            }
+        });
+    }
+}
+
 class Cube {
     constructor() {
         const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -94,35 +74,29 @@ class Cube {
 
 
 function main() {
-    const app = new GameApp;
+    const controller = new Controller;
+    const app = new GameApp(controller);
     app.init();
+    app.controller.handleKeyInput();
 
     loop();
 
     function loop() {
-
-
-        if (keyArray[KEY_LEFT]) {
+        if (app.controller.moveKeys.get(37)) { // Left
             app.camera.position.x -= 0.1
         }
-        if (keyArray[KEY_RIGHT]) {
+        if (app.controller.moveKeys.get(39)) { // Right
             app.camera.position.x += 0.1
         }
-        if (keyArray[KEY_UP]) {
+        if (app.controller.moveKeys.get(38)) { // Forward
             app.camera.position.z -= 0.1
         }
-        if (keyArray[KEY_DOWN]) {
+        if (app.controller.moveKeys.get(40)) { // Back
             app.camera.position.z += 0.1
         }
 
-        for (let i = 0; i < 4; ++i) {
-            if (keyArrayUp[i]) {// キーリピート OFF
-                keyArray[i] = false;
-                keyArrayUp[i] = false;            
-            }
-        }
-
         app.render();
+        
         requestAnimationFrame( loop );
     }
 }
