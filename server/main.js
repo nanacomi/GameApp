3d7ba7fbd2151072e330af9ccdcccea04916b5cb
Masato Ebisawa <j0td5ta0d1mt@icloud.com>
2019-07-11 00:34:38 +0900

    fix websocket bugs and set no-cache

--- server/main.js
+++ server/main.js
@@ -7,23 +7,26 @@ const io = require('socket.io')(http);
 const users = new Map();
 
 io.on('connection', socket => {
-    console.log('connected');
-
     socket.on('connected', data => {
-        io.emit('connected', {
-            newUser: data,
-            currentUsers: Array.from(users.values())
-        });
-
         users.set(socket.id, {
-            id: data.id,
+            clientID: data.clientID,
             position: data.position
         });
+
+        io.emit('connected', {
+            newUser: data,
+            currentUsers: Array.from(users.values()).filter(user => user.clientID !== data.clientID)
+        });
+
+        console.log('new client connected:');
+        console.log(Array.from(users.values()).reduce((acc, c) => {
+            return acc + (c.clientID === data.clientID ? `\u001b[36m${c.clientID}\u001b[0m, ` : `${c.clientID}, `)
+        }, '').slice(0, -2))
     });
 
     socket.on('updatePosition', data => {
         users.set(socket.id, {
-            id: data.id,
+            clientID: data.clientID,
             position: data.position
         });
         io.emit('updatePosition', data);
@@ -31,8 +34,12 @@ io.on('connection', socket => {
 
     socket.on("disconnect",  () => {
         if (users.has(socket.id)) {
-            io.emit('disconnected', users.get(socket.id).id);
+            io.emit('disconnected', users.get(socket.id).clientID);
             users.delete(socket.id);
+            console.log('client disconnected:');
+            console.log(Array.from(users.values()).reduce((acc, c) => {
+                return acc + c.clientID + ', '
+            }, '').slice(0, -2));
         }
     });
 });
