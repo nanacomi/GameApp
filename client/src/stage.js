16a3b74bf14dd207b06853ddc9ded2226f7eaa5d
宮本七々子 <naaaaaaa.tooo@gmail.com>
2019-09-01 20:14:07 +0900

    add: multi-floor

--- client/src/stage.js
+++ client/src/stage.js
@@ -15,5 +15,138 @@ export default class Stage {
                 scene.add( this.cubes[this.cubes.length - 1].mesh );
             }
         }
+
+        // 2階
+        for (let x = 0; x < MAX; ++x) {
+            for (let z = 0; z < MAX; ++z) {
+                this.cubes.push( new Cube() );
+                this.cubes[this.cubes.length - 1].mesh.position.x = x * 1.2;
+                this.cubes[this.cubes.length - 1].mesh.position.y = 10.0;
+                this.cubes[this.cubes.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.cubes[this.cubes.length - 1].mesh );
+            }
+        }
+
+        // 3階
+        for (let x = 0; x < MAX; ++x) {
+            for (let z = 0; z < MAX; ++z) {
+                this.cubes.push( new Cube() );
+                this.cubes[this.cubes.length - 1].mesh.position.x = x * 1.2;
+                this.cubes[this.cubes.length - 1].mesh.position.y = 20.0;
+                this.cubes[this.cubes.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.cubes[this.cubes.length - 1].mesh );
+            }
+        }
+
+        // 4階
+        for (let x = 0; x < MAX; ++x) {
+            for (let z = 0; z < MAX; ++z) {
+                this.cubes.push( new Cube() );
+                this.cubes[this.cubes.length - 1].mesh.position.x = x * 1.2;
+                this.cubes[this.cubes.length - 1].mesh.position.y = 30.0;
+                this.cubes[this.cubes.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.cubes[this.cubes.length - 1].mesh );
+            }
+        }
+
+        this.stairs = [];
+        // 1階から2階
+        for (let x = 30; x < 32; ++x) {
+            for (let z = 30; z < 32; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 2.5;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+        for (let x = 28; x < 30; ++x) {
+            for (let z = 28; z < 30; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 5.0;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+        for (let x = 26; x < 28; ++x) {
+            for (let z = 26; z < 28; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 7.5;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+
+        // 2階から3階
+        for (let x = 30; x < 32; ++x) {
+            for (let z = 30; z < 32; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 12.5;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+        for (let x = 28; x < 30; ++x) {
+            for (let z = 28; z < 30; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 15.0;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+        for (let x = 26; x < 28; ++x) {
+            for (let z = 26; z < 28; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 17.5;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+
+        // 3階から4階
+        for (let x = 30; x < 32; ++x) {
+            for (let z = 30; z < 32; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 22.5;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+        for (let x = 28; x < 30; ++x) {
+            for (let z = 28; z < 30; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 25.0;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
+        for (let x = 26; x < 28; ++x) {
+            for (let z = 26; z < 28; ++z) {
+                this.stairs.push( new Cube() );
+                this.stairs[this.stairs.length - 1].mesh.position.x = x * 1.2;
+                this.stairs[this.stairs.length - 1].mesh.position.y = 27.5;
+                this.stairs[this.stairs.length - 1].mesh.position.z = z * 1.2;
+                // 3D空間にメッシュを追加
+                scene.add( this.stairs[this.stairs.length - 1].mesh );
+            }
+        }
     }
 }
